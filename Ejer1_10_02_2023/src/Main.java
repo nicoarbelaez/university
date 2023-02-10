ba18dc7238254b8d41da6d28ae666f4ef876f40b
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 16:32:42 -0500

    2.0.0

--- Ejer1_10_02_2023/src/Main.java
+++ Ejer1_10_02_2023/src/Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.print(ContarPalabras());
+	}
+	
+	public static String ContarPalabras() {
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.print("Ingrese su cadena de texto: ");
-		String nombre = sc.next();
-		System.out.print("La cadena tiene " + nombre.length() + " letras.");
+		String string = sc.next();
 		
 		sc.close();
+		return "La cadena tiene " + string.length() + " letras.";
 	}
 }
