43632dd1e931747300e72b849bc68073ae87ee6f
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 16:03:17 -0500

    Guardar

--- Ejer1_10_02_2023/src/Main.java
+++ Ejer1_10_02_2023/src/Main.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese su cadena de texto: ");
+		String nombre = sc.next();
+		System.out.print("La cadena tiene " + nombre.length() + " letras.");
+		
+		sc.close();
+	}
 }
