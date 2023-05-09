9e3876aec2fa802e5f3d90b678cfc4c84e3f2058
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2023-05-09 10:51:38 -0500

    taller11

--- taller11/src/pointFour/PointFour.java
+++ taller11/src/pointFour/PointFour.java
@@ -1,5 +1,26 @@
 package pointFour;
 
 public class PointFour {
+    public int num = 5;
 
+    public PointFour() {
+        System.out.println(toString());
+    }
+
+    public int getFactorial() {
+        int factorial = 1;
+        for (int i = this.num; i > 1; i--) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public String toString() {
+        String result = "Factorial: " + this.num +"! = ";
+        for (int i = 1; i <= this.num; i++) {
+            result += i + " * ";
+        }
+        result = result.substring(0, result.length() - 2) + "= " + getFactorial();
+        return result;
+    }
 }
--- taller11/src/pointThree/PointThree.java
+++ taller11/src/pointThree/PointThree.java
@@ -2,4 +2,22 @@ package pointThree;
 
 public class PointThree {
 
+    public PointThree() {
+        System.out.println("A a= new...");
+        System.out.println("B b= new...");
+        System.out.println("C c= new...");
+        System.out.println("D d= new...");
+        System.out.println("E e= new...");
+        System.out.println("Se incluyen los siguientes métodos:");
+        System.out.println("interfaz A especifica method void methodA()");
+        System.out.println("clase C tiene el abstract method void methodC()");
+
+        System.out.println("\tCodigo: \t ¿Siempre compila, a veces compila, no compila?");
+        System.out.println("a = new B(); \t\t\t Simpre compila");
+        System.out.println("d = new C(); \t\t\t No compila");
+        System.out.println("b.methodA(); \t\t\t A veces compila");
+        System.out.println("e.methodA(); \t\t\t Siempre compila");
+        System.out.println("c = new C(); \t\t\t A veces compila");
+        System.out.println("(D)c.methodC(); \t\t A veces compila");
+    }
 }
